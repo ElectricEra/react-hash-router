42d0ad2a48ca40e648a656b903d7bb645c8a2751
Charles Luxton <charles.g.luxton@gmail.com>
2015-11-07 17:49:42 +1000

    Using 'query-string' package to parse query strings

--- index.js
+++ index.js
@@ -22,6 +22,5 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 module.exports = require('./lib/Router');
-
 module.exports.RouteHandler = require('./lib/RouteHandler');
 module.exports.Path = require('./lib/Path');
